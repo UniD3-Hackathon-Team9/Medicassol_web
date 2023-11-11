568d8e1524a23505f27a29f50db2d88f9dae22a3
byeori-moon <mblee2@naver.com>
2023-11-12 03:38:38 +0900

    모달창

--- medicassol-web/src/Page2.jsx
+++ medicassol-web/src/Page2.jsx
@@ -1,64 +1,83 @@
-import React from "react";
+import React, {useState} from "react";
 import * as S from "./Page2.style";
 
+const Modal = ({ onClose, show, setTableData, tableData }) => {
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+  const [medication, setMedication] = useState("");
+
+  const handleSubmit = () => {
+    const newEntry = {date, time, medication};
+    setTableData([...tableData, newEntry]);
+    onClose(); // 모달 닫기
+  };
+
+  if (!show) {
+    return null;
+  }
+
+  return (
+    <S.Modal>
+      <S.ModalContent>
+        <S.Close onClick={onClose}>&times;</S.Close>
+        <input
+          type="date"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+        />
+        <input
+          type="time"
+          value={time}
+          onChange={(e) => setTime(e.target.value)}
+          step="600" // 10분 단위로 설정
+        />
+        <input
+          type="text"
+          placeholder="Medication Name"
+          value={medication}
+          onChange={(e) => setMedication(e.target.value)}
+        />
+        <button onClick={handleSubmit}>Submit</button>
+      </S.ModalContent>
+    </S.Modal>
+  );
+};
+
 function Page2() {
+  const [showModal, setModalShow] = useState(false);
+  const [tableData, setTableData] = useState([]);
   return (
     <S.PageContainer>
       <S.Brain src="/images/brain.png"></S.Brain>
+      <div>
+        <button onClick={() => setModalShow(true)}>투약정보작성</button>
+        <Modal
+          onClose={() => setModalShow(false)}
+          show={showModal}
+          setTableData={setTableData}
+          tableData={tableData}
+        />
+      </div>
+
       <S.ScrollView>
-      <S.Medic>
+        <S.Medic>
           <tr>
             <S.Th>투여할 시간</S.Th>
             <S.Th>투여할 약물이름</S.Th>
             <S.Th>약물투여완료여부</S.Th>
           </tr>
-          <tr>
-            <S.Td>2023-02-11</S.Td>
-            <S.Td>ab</S.Td>
-            <S.Td>투여 완료</S.Td>
-          </tr>
-          <tr>
-            <S.Td>2023-02-11</S.Td>
-            <S.Td>ab</S.Td>
-            <S.Td>투여 완료</S.Td>
-          </tr>
-          <tr>
-            <S.Td>2023-02-11</S.Td>
-            <S.Td>ab</S.Td>
-            <S.Td>투여 완료</S.Td>
-          </tr>
-          <tr>
-            <S.Td>2023-02-11</S.Td>
-            <S.Td>ab</S.Td>
-            <S.Td>투여 완료</S.Td>
-          </tr>
-          <tr>
-            <S.Td>2023-02-11</S.Td>
-            <S.Td>ab</S.Td>
-            <S.Td>투여 완료</S.Td>
-          </tr>
-          <tr>
-            <S.Td>2023-02-11</S.Td>
-            <S.Td>ab</S.Td>
-            <S.Td>투여 완료</S.Td>
-          </tr>
-          <tr>
-            <S.Td>2023-02-11</S.Td>
-            <S.Td>ab</S.Td>
-            <S.Td>투여 완료</S.Td>
-          </tr>
-          <tr>
-            <S.Td>2023-02-11</S.Td>
-            <S.Td>ab</S.Td>
-            <S.Td>투여 완료</S.Td>
-          </tr>
-          <tr>
-            <S.Td>2023-02-11</S.Td>
-            <S.Td>ab</S.Td>
-            <S.Td>투여 완료</S.Td>
-          </tr>
-        </S.Medic></S.ScrollView>
-        <S.ScrollView>
+          {tableData.map((entry, index) => (
+            <tr key={index}>
+              <S.Td>
+                {entry.date} {entry.time}
+              </S.Td>
+              <S.Td>{entry.medication}</S.Td>
+              <S.Td></S.Td>
+            </tr>
+          ))}
+        </S.Medic>
+      </S.ScrollView>
+      <S.ScrollView>
         <S.Medic>
           <tr>
             <S.Th>투여한 시간</S.Th>
@@ -100,7 +119,8 @@ function Page2() {
             <S.Td>2023-02-11</S.Td>
             <S.Td>ab</S.Td>
           </tr>
-      </S.Medic></S.ScrollView>
+        </S.Medic>
+      </S.ScrollView>
     </S.PageContainer>
   );
 }
--- medicassol-web/src/Page2.style.jsx
+++ medicassol-web/src/Page2.style.jsx
@@ -9,7 +9,7 @@ export const PageContainer = styled.div`
 export const Brain = styled.img`
   border: 1px solid black;
   margin-bottom: 10px;
-  margin-top: 10px; // Corrected margin-top value
+  margin-top: 10px;
   height: 200px;
 `;
 
@@ -18,29 +18,55 @@ export const Medic = styled.table`
   height: 100px;
   border-collapse: collapse;
   margin-left: 10px;
-  overflow:scroll;
+  overflow: scroll;
 `;
 
-export const ScrollView=styled.div`
-border: 1px solid;
-width:400px;
-height: 30vh;
-margin-bottom:20px;
+export const ScrollView = styled.div`
+  border: 1px solid;
+  width: 400px;
+  height: 30vh;
+  margin-bottom: 20px;
 
-overflow-y: auto; // 세로 스크롤 적용
-overflow-x: hidden; // 가로 스크롤 숨김
-
+  overflow-y: auto; // 세로 스크롤 적용
+  overflow-x: hidden; // 가로 스크롤 숨김
 `;
 
 export const Th = styled.th`
   border: 1px solid #ccc;
   padding: 5px;
   background-color: #f2f2f2;
-  position:sticky;
-  top:0px;
+  position: sticky;
+  top: 0px;
 `;
 
 export const Td = styled.td`
   border: 1px solid #ccc;
   padding: 5px;
+  z-index: 1000; 
 `;
+
+export const Modal = styled.div`
+  position: fixed;
+  background-color: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000; 
+`;
+
+export const ModalContent = styled.div`
+  background-color: white;
+  padding: 20px;
+  border-radius: 5px;
+  z-index: 1000; 
+  display:flex;
+  flex-direction:column;
+  gap:10px;
+`;
+
+export const Close = styled.span`
+  float: right;
+  cursor: pointer;
+  z-index: 1000; 
+`;
+
