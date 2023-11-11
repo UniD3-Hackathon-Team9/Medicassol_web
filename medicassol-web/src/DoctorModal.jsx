391a0d97b4b0dce0e465c004124f93619d84eccf
chan_nie <126068623+chennielee@users.noreply.github.com>
2023-11-12 08:30:13 +0900

    ㄹㄹ

--- medicassol-web/src/DoctorModal.jsx
+++ medicassol-web/src/DoctorModal.jsx
@@ -3,65 +3,63 @@ import React, { useState } from "react";
 import * as S from "./DoctorModal.style";
 import axios from "axios";
 
-function DoctorModal({ onClose, onSubmit }) {
-  const response = {
-    tokens: {
-      accessToken:
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoiYWNjZXNzIiwidXNlcklkIjoidGVzdDEyMzQiLCJ1c2VyVHlwZSI6ImRvY3RvciIsImlhdCI6MTY5OTczMzYwNiwiZXhwIjoxNzMxMjkxMjA2fQ.ybaU2jdQ2HIL97Z9oFFkH6_2sld-E8LU2KV8Vay-eWI",
-    },
-    status: "200",
+const response = {
+  tokens: {
+    accessToken:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoiYWNjZXNzIiwidXNlcklkIjoidGVzdDEyMzQiLCJ1c2VyVHlwZSI6ImRvY3RvciIsImlhdCI6MTY5OTczMzYwNiwiZXhwIjoxNzMxMjkxMjA2fQ.ybaU2jdQ2HIL97Z9oFFkH6_2sld-E8LU2KV8Vay-eWI",
+  },
+  status: "200",
+};
+function DoctorModal({ onClose, onSubmit, onInputChange }) {
+  const [userInput, setUserInput] = useState("");
+
+  const handleInputChange = (e) => {
+    setUserInput(e.target.value);
   };
-  function DoctorModal({ onClose, onSubmit, onInputChange }) {
-    const [userInput, setUserInput] = useState("");
 
-    const handleInputChange = (e) => {
-      setUserInput(e.target.value);
-    };
+  const handleSubmit = () => {
+    if (userInput.trim() !== "") {
+      onSubmit(userInput);
 
-    const handleSubmit = () => {
-      if (userInput.trim() !== "") {
-        onSubmit(userInput);
+      const userMessage = {
+        patientIdx: 1,
+        message: userInput,
+      };
+      axios
+        .post("https://server.medicassol.info/doctor/message", userMessage, {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${response.tokens.accessToken}`,
+          },
+        })
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.error("에러:", error);
+        });
+      onSubmit();
 
-        const userMessage = {
-          patientIdx: 1,
-          message: userInput,
-        };
-        axios
-          .post("https://server.medicassol.info/doctor/message", userMessage, {
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${response.tokens.accessToken}`,
-            },
-          })
-          .then((response) => {
-            console.log(response.data);
-          })
-          .catch((error) => {
-            console.error("에러:", error);
-          });
-        onSubmit();
+      setUserInput("");
 
-        setUserInput("");
+      onClose();
+    }
+  };
 
-        onClose();
-      }
-    };
-
-    return (
-      <S.ModalContainer>
-        <S.ModalContent>
-          <input
-            type="text"
-            placeholder="Type your message here"
-            value={userInput}
-            onChange={handleInputChange}
-          />
-          <button onClick={handleSubmit}>추가하기</button>
-          <button onClick={onClose}>취소하기</button>
-        </S.ModalContent>
-      </S.ModalContainer>
-    );
-  }
+  return (
+    <S.ModalContainer>
+      <S.ModalContent>
+        <input
+          type="text"
+          placeholder="Type your message here"
+          value={userInput}
+          onChange={handleInputChange}
+        />
+        <button onClick={handleSubmit}>추가하기</button>
+        <button onClick={onClose}>취소하기</button>
+      </S.ModalContent>
+    </S.ModalContainer>
+  );
 }
 
 export default DoctorModal;
--- medicassol-web/src/Page1.jsx
+++ medicassol-web/src/Page1.jsx
@@ -1,8 +1,7 @@
 // Page1.jsx
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import * as S from "./Page1.style";
 import DoctorModal from "./DoctorModal";
-import axios from "axios";
 
 const response = {
   tokens: {
@@ -15,70 +14,62 @@ const response = {
 function Page1() {
   const [postedMessages, setPostedMessages] = useState([]);
   const [showModal, setShowModal] = useState(false);
+  const [modalMessage, setModalMessage] = useState("");
 
   const handlePostMessage = (message) => {
     setPostedMessages([...postedMessages, message]);
-
-    const handlePostMessage = () => {
-      setPostedMessages([...postedMessages, modalMessage]);
-
-      setModalMessage("");
-
-      handleCloseModal();
-    };
-
-    const handleShowModal = () => {
-      setShowModal(true);
-    };
-
-    const handleCloseModal = () => {
-      setShowModal(false);
-    };
-
-    const handleModalInputChange = (message) => {
-      setModalMessage(message);
-    };
-
-    return (
-      <>
-        <S.Container>
-          <S.DoctorBox>
-            {postedMessages.map((message, index) => (
-              <S.GetContent key={index}>{message}</S.GetContent>
-            ))}
-          </S.DoctorBox>
-          {!showModal && (
-            <button
-              onClick={handleShowModal}
-              style={{
-                marginTop: "5px",
-                backgroundColor: "#4caf50",
-                color: "white",
-                border: "none",
-                borderRadius: "5px",
-                cursor: "pointer",
-
-                transition: "background-color 0.3s",
-                ":active": {
-                  backgroundColor: "#45a049", // Darken the color on click
-                },
-              }}
-            >
-              추가소견 작성
-            </button>
-          )}
-          {showModal && (
-            <DoctorModal
-              onClose={handleCloseModal}
-              onSubmit={handlePostMessage}
-            />
-          )}
-
-          <S.PatientMessage></S.PatientMessage>
-        </S.Container>
-      </>
-    );
+    setModalMessage("");
+    handleCloseModal();
   };
+
+  const handleShowModal = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  const handleModalInputChange = (message) => {
+    setModalMessage(message);
+  };
+
+  return (
+    <>
+      <S.Container>
+        <S.DoctorBox>
+          {postedMessages.map((message, index) => (
+            <S.GetContent key={index}>{message}</S.GetContent>
+          ))}
+        </S.DoctorBox>
+        {!showModal && (
+          <button
+            onClick={handleShowModal}
+            style={{
+              marginTop: "5px",
+              backgroundColor: "#4caf50",
+              color: "white",
+              border: "none",
+              borderRadius: "5px",
+              cursor: "pointer",
+              transition: "background-color 0.3s",
+            }}
+          >
+            추가소견 작성
+          </button>
+        )}
+        {showModal && (
+          <DoctorModal
+            onClose={handleCloseModal}
+            onSubmit={handlePostMessage}
+            onInputChange={handleModalInputChange}
+          />
+        )}
+
+        <S.PatientMessage>{modalMessage}</S.PatientMessage>
+      </S.Container>
+    </>
+  );
 }
 
 export default Page1;
